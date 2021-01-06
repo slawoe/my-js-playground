d52437c3e9a8a43c739599dd6e9f64e34a1e5071
Slawomir Ernst <info@slawomirernst.de>
2021-01-06 21:04:50 +0100

    Delete lottozahlen file and restructure completely lottNumbers file

--- Lotto/lottoNumbers.js
+++ Lotto/lottoNumbers.js
@@ -1,133 +1,64 @@
-let lottoNumbers = [];
 let numberOfFields;
 let system;
-const zusatzzahl = Math.floor(Math.random() * 10);
 const numbers = document.querySelector("#numbers");
 
 function whichSystem() {
-  systemAnswer = prompt("Welches System wird gespielt? Lotto oder Euro?");
+  systemAnswer = prompt("Welches System spielst Du? Lotto oder Euro?");
   system = systemAnswer.toLowerCase();
 }
 whichSystem();
 
-if (system === "lotto") {
-  function howManyFields() {
-    numberOfFields = prompt(
-      "Willkommen beim Lotto Generator. Wie viele Felder dürfen es sein?"
-    );
-    if (numberOfFields === "") {
-      numberOfFields = 12;
-    }
+function howManyFields() {
+  numberOfFields = prompt("Wie viele Felder sollen gespielt werden?");
+  if (numberOfFields === "") {
+    numberOfFields = 12;
   }
-  howManyFields();
+}
 
-  for (let i = 0; i < numberOfFields; i++) {
-    for (let i = 0; i < 6; i++) {
-      lottoNumbers[i] = Math.floor(Math.random() * 49 + 1);
-      if (lottoNumbers[0] === lottoNumbers[1]) {
-        lottoNumbers[1]++;
-      } else if (
-        lottoNumbers[0] === lottoNumbers[2] ||
-        lottoNumbers[1] === lottoNumbers[2]
-      ) {
-        lottoNumbers[2]++;
-      } else if (
-        lottoNumbers[0] === lottoNumbers[3] ||
-        lottoNumbers[1] === lottoNumbers[3] ||
-        lottoNumbers[2] === lottoNumbers[3]
-      ) {
-        lottoNumbers[3]++;
-      } else if (
-        lottoNumbers[0] === lottoNumbers[4] ||
-        lottoNumbers[1] === lottoNumbers[4] ||
-        lottoNumbers[2] === lottoNumbers[4] ||
-        lottoNumbers[3] === lottoNumbers[4]
-      ) {
-        lottoNumbers[4]++;
-      } else if (
-        lottoNumbers[0] === lottoNumbers[5] ||
-        lottoNumbers[1] === lottoNumbers[5] ||
-        lottoNumbers[2] === lottoNumbers[5] ||
-        lottoNumbers[3] === lottoNumbers[5] ||
-        lottoNumbers[4] === lottoNumbers[5]
-      ) {
-        lottoNumbers[5]++;
-      } else if (lottoNumbers[i] === 50) {
-        lottoNumbers[i] -= Math.floor(Math.random() * 48 + 1);
-      }
-      lottoNumbers[i] = ("0" + lottoNumbers[i]).slice(-2);
-    }
-    lottoNumbers.sort();
-    const numbersDIV = document.createElement("span");
-    numbersDIV.innerText = `Feld ${[i + 1]}: ${lottoNumbers}`;
-    numbers.append(numbersDIV);
-  }
-  const numbersDIV = document.createElement("span");
-  numbersDIV.innerText = `Zusatzzahl lautet: ${zusatzzahl}`;
-  numbers.append(numbersDIV);
-} else if (system === "euro") {
-  function howManyFields() {
-    numberOfFields = prompt(
-      "Willkommen beim EuroJackpot-Generator. Wie viele Felder dürfen es sein?"
-    );
-    if (numberOfFields === "") {
-      numberOfFields = 12;
-    }
-  }
-  howManyFields();
-
-  for (let i = 0; i < numberOfFields; i++) {
-    for (let i = 0; i < 5; i++) {
-      lottoNumbers[i] = Math.floor(Math.random() * 50 + 1);
-      if (lottoNumbers[0] === lottoNumbers[1]) {
-        lottoNumbers[1]++;
-      } else if (
-        lottoNumbers[0] === lottoNumbers[2] ||
-        lottoNumbers[1] === lottoNumbers[2]
-      ) {
-        lottoNumbers[2]++;
-      } else if (
-        lottoNumbers[0] === lottoNumbers[3] ||
-        lottoNumbers[1] === lottoNumbers[3] ||
-        lottoNumbers[2] === lottoNumbers[3]
-      ) {
-        lottoNumbers[3]++;
-      } else if (
-        lottoNumbers[0] === lottoNumbers[4] ||
-        lottoNumbers[1] === lottoNumbers[4] ||
-        lottoNumbers[2] === lottoNumbers[4] ||
-        lottoNumbers[3] === lottoNumbers[4]
-      ) {
-        lottoNumbers[4]++;
-      } else if (lottoNumbers[i] === 51) {
-        lottoNumbers[i] -= Math.floor(Math.random() * 49 + 1);
-      }
-      lottoNumbers[i] = ("0" + lottoNumbers[i]).slice(-2);
-    }
-    lottoNumbers.sort();
-    const numbersDIV = document.createElement("span");
-    numbersDIV.innerText = `Feld ${[i + 1]}: ${lottoNumbers}`;
-    numbers.append(numbersDIV);
-    let zusatzzahl = Math.floor(Math.random() * 10);
-    let zusatzzahl2 = Math.floor(Math.random() * 10);
-    if (zusatzzahl === zusatzzahl2) {
-      const numbersDIV = document.createElement("span");
-      numbersDIV.innerText = `Zusatzzahlen lauten: ${zusatzzahl} + ${zusatzzahl2++}`;
-      numbers.append(numbersDIV);
-    } else if (zusatzzahl2 === "10") {
-      const numbersDIV = document.createElement("span");
-      numbersDIV.innerText = `Zusatzzahlen lauten: ${zusatzzahl} + ${(zusatzzahl2 -= Math.floor(
-        Math.random() * 8 + 1
-      ))}`;
-      numbers.append(numbersDIV);
+function genreateNumbers(length, numberOfNumbers) {
+  let numbers = [];
+  for (let i = 0; numbers.length < length; i++) {
+    let number = Math.floor(Math.random() * numberOfNumbers + 1);
+    if (numbers.includes(number)) {
+      number = Math.floor(Math.random() * numberOfNumbers + 1);
     } else {
-      const numbersDIV = document.createElement("span");
-      numbersDIV.innerText = `Die Zusatzzahlen lauten für Feld ${[
-        i + 1,
-      ]}: ${zusatzzahl} + ${zusatzzahl2}`;
-      numbers.append(numbersDIV);
+      numbers.push(" " + number);
+      numbers.sort((a, b) => {
+        return a - b;
+      });
     }
   }
-} else {
-  console.log("System ist unbekannt");
+  return numbers;
+}
+
+switch (system) {
+  case "lotto":
+    howManyFields();
+    for (let i = 0; i < numberOfFields; i++) {
+      const numbersDIV = document.createElement("span");
+      numbersDIV.innerText = `Feld ${[i + 1]}: ${genreateNumbers(6, 49)}`;
+      numbers.append(numbersDIV);
+    }
+    const numbersDIV = document.createElement("span");
+    numbersDIV.innerText = `Die Zusatzzahl lautet: ${Math.floor(
+      Math.random() * 9 + 1
+    )}`;
+    numbers.append(numbersDIV);
+    break;
+  case "euro":
+    howManyFields();
+    for (let i = 0; i < numberOfFields; i++) {
+      const numbersDIV = document.createElement("span");
+      const additionalnumbersDIV = document.createElement("span");
+      numbersDIV.innerText = `Feld ${[i + 1]}: ${genreateNumbers(5, 50)}`;
+      additionalnumbersDIV.innerText = `Zusatzzahlen lauten: ${genreateNumbers(
+        2,
+        10
+      )}`;
+      numbers.append(numbersDIV);
+      numbers.append(additionalnumbersDIV);
+    }
+    break;
+  default:
+    prompt("System ist unbekannt. Bitte aktualisiere das Fenster!");
 }
