be791f6988262d51dcf8fc5256d9da97a1480607
Slawomir Ernst <info@slawomirernst.de>
2021-01-11 17:44:21 +0100

    Add split by new lines

--- WordCounter/wordCounter.js
+++ WordCounter/wordCounter.js
@@ -2,6 +2,8 @@ const textarea = document.querySelector("textarea");
 
 function counter() {
   let text = textarea.value;
+  text = text.split(`\n`);
+  text = text.join(" ");
   text = text.split(" ");
   console.log(text);
   console.log(text.length);
